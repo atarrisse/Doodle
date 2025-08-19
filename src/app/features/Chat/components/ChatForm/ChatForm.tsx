432a2a83a1c6e67e8095d417144e1066f3920a7a
Ana Tarrisse <atarrisse@gmail.com>
2025-08-19 20:40:26 +0200

    ✨ ChatForm component

---
+++ src/app/features/Chat/components/ChatForm/ChatForm.tsx
@@ -0,0 +1,37 @@
+"use client";
+import Button from "@/app/components/Button/Button";
+import Input from "@/app/components/Input";
+import React, { useState } from "react";
+
+const ChatInput: React.FC = ({}) => {
+  const [message, setMessage] = useState("");
+
+//   const handleSubmit = (e: FormEvent) => {
+//     e.preventDefault();
+//     if (message.trim()) {
+//       console.log(message.trim());
+//       setMessage("");
+//     }
+//   };
+
+  return (
+    <div className="bg-(--color-blue) w-full py-2">
+      <div className="max-w-(--container-w) m-auto flex align-center justify-center px-6">
+        <form className="w-full flex gap-2">
+          <Input
+            id="chat-input"
+            label="Write a message"
+            placeholder="Message"
+            className="grow"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button type="submit" disabled={!message.trim()}>
+            Send
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ChatInput;
