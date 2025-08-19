f1d0353fce1c3fa0cfabc414a93623c1ca4415b0
Ana Tarrisse <atarrisse@gmail.com>
2025-08-19 17:11:24 +0200

    ✨ connect data to component

--- src/app/features/Chat/components/ChatMessages/ChatMessages.tsx
+++ src/app/features/Chat/components/ChatMessages/ChatMessages.tsx
@@ -1,48 +1,18 @@
 "use client";
-import { useState, useEffect } from "react";
+import useChatMessages from "../../hooks/useChatMessages";
+import ChatBubble from "../ChatBubble";
 
 function ChatMessages() {
-  const [messages, setMessages] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchMessages = async () => {
-      try {
-        const response = await fetch("http://localhost:3000/api/v1/messages", {
-          method: "GET",
-          headers: {
-            Authorization: "Bearer super-secret-doodle-token",
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        setMessages(data); // store messages in state
-      } catch (err) {
-        setError(err?.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchMessages();
-  }, []); // empty dependency array = run once on mount
+  const { messages, loading, error } = useChatMessages();
 
   if (loading) return <p>Loading messages...</p>;
-  if (error) return <p style={{ color: "red" }}>Error: {error}</p>;
+  if (error) return <p style={{ color: "red" }}>Error: {error.message}</p>;
 
   return (
     <div>
-      <h2>Messages</h2>
-      <ul>
-        {messages.map((msg, index) => (
-          <li key={index}>{JSON.stringify(msg)}</li>
+        {messages.map((msg) => (
+          <ChatBubble key={msg._id} message={msg.message} createdAt={msg.createdAt} author={msg.author} />
         ))}
-      </ul>
     </div>
   );
 }
