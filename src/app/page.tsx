e9b7ff358e09e162cfc4fe5789191fa9f0d64a22
Ana Tarrisse <atarrisse@gmail.com>
2025-08-19 18:53:27 +0200

    💄 positioning of Chat feature on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,16 @@ import Chat from "@/app/features/Chat";
 export default function Home() {
   return (
     <main className="flex flex-col gap-[32px] items-center ">
-      <Image alt="" src="/background.png" objectFit="cover" fill={true} className="z-[-1]"/>
-      <Chat />
+      <Image
+        alt=""
+        src="/background.png"
+        objectFit="cover"
+        fill={true}
+        className="z-[-1]"
+      />
+      <section className="flex flex-colitems-center w-full max-w-[40rem]">
+        <Chat />
+      </section>
     </main>
   );
 }
