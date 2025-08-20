5f03969eca9eead5a33458fd752dd5dfab158653
Ana Tarrisse <atarrisse@gmail.com>
2025-08-20 17:22:17 +0200

    🔧 Jest aliases

--- jest.config.ts
+++ jest.config.ts
@@ -11,7 +11,11 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ['./jest.setup.ts']
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  moduleNameMapper: {
+  // ...
+  '^@/(.*)$': '<rootDir>/src/$1',
+}
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
