ec3d5a3f625e55199f8dab4769d024f57b80349c
Ana Tarrisse <atarrisse@gmail.com>
2025-08-20 04:10:59 +0200

    🗑️ remove bloar

--- src/app/features/Chat/hooks/useChatMessages.tsx
+++ src/app/features/Chat/hooks/useChatMessages.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-import { ChatMessage, fetchMessages } from "@/app/data";
+import { ChatMessage, createMessage, fetchMessages } from "@/app/data";
 
 const useChatMessages = () => {
   const [messages, setMessages] = useState<Array<ChatMessage>>([]);
@@ -8,28 +8,32 @@ const useChatMessages = () => {
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    let isMounted = true;
     setLoading(true);
 
-    fetchMessages()
-      .then((data) => {
-        if (isMounted) {
-          setMessages(data);
-          setLoading(false);
-        }
-      })
-      .catch((err) => {
-        if (isMounted) {
-          setError(err);
-          setLoading(false);
-        }
-      });
-    return () => {
-      isMounted = false;
-    };
+    fetchMessages().then((data) => {
+      setMessages(data);
+      setLoading(false);
+    } ).catch((err) => {
+      setError(err as Error);
+      setLoading(false);
+    });
   }, []);
 
-  return { messages, loading, error };
+  const sendMessage = async (message: string) => {
+    setLoading(true);
+
+    try {
+      const newMessage = await createMessage(message);
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+      setLoading(false);
+      return messages;
+    } catch (err) {
+      setError(err as Error);
+      setLoading(false);
+    }
+  };
+
+  return { messages, loading, error, sendMessage };
 };
 
 export default useChatMessages;
